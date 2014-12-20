27b1dd36e442cdf80ec30e056bb69f2c7a65ffce
vacho <vacho@koala-soft.com>
2014-12-20 11:08:08 -0400

    Refactorizamos el código

--- js/calculator.js
+++ js/calculator.js
@@ -1,103 +1,101 @@
 (function ($) {
-	var nuevo = true;
-
+    var nuevo = true;
     $(document).ready(function() {
-		
-	   /*
-	    * Aqui se escribe cada numero que haga click
-	    */
-       for(i=0; i<10; i++){
-           var obj = '#edit-' + i;
-           $(document).on("click",obj,function(event){ 
-             if(nuevo)
-               var num = $('#edit-display').val() + this.value;
-             else{
-               var num = this.value;
-               nuevo = true;
-             }
-               $('#edit-display').val(num);
-               event.preventDefault();
-           });
-       }
-       /*
-	    * Es el punto para decimales.
-	    */
-       $('#op-pto').click(function(event){
-           if(nuevo)
-		      var num = $('#edit-display').val() + '.';
-		   else {
-		      num = '.';
-		      nuevo = true;
-		   }
-           $('#edit-display').val(num);
-           event.preventDefault();
-       });
+
+        /*
+        * Aqui se escribe cada numero que haga click
+        */
+        for(i=0; i<10; i++){
+            var obj = '#edit-' + i;
+            $(document).on("click",obj,function(event){ 
+                if(nuevo)
+                    var num = $('#edit-display').val() + this.value;
+                else{
+                    var num = this.value;
+                    nuevo = true;
+                }
+                $('#edit-display').val(num);
+                event.preventDefault();
+            });
+        }
+        /*
+        * Es el punto para decimales.
+        */
+        $('#op-pto').click(function(event){
+            if(nuevo)
+            var num = $('#edit-display').val() + '.';
+        else {
+            num = '.';
+            nuevo = true;
+        }
+            $('#edit-display').val(num);
+            event.preventDefault();
+        });
        
-       /*
-	    * 'AC' Limpia toda nuestra pantalla
-	    */
-       $('#op-AC').click(function(event){
-           $('#edit-display').val('');
-           event.preventDefault();
-       });
-		
-		/*
-	    * '+' La operacion de suma.
-	    */
-       $('#op-plus').click(function(event){
-           $('#edit-display').val($('#edit-display').val() + '+') ;
-           nuevo = true;
-           event.preventDefault();
-       });
+        /*
+        * 'AC' Limpia toda nuestra pantalla
+        */
+        $('#op-AC').click(function(event){
+            $('#edit-display').val('');
+            event.preventDefault();
+        });
+    
+        /*
+        * '+' La operacion de suma.
+        */
+        $('#op-plus').click(function(event){
+            $('#edit-display').val($('#edit-display').val() + '+') ;
+            nuevo = true;
+            event.preventDefault();
+        });
        
-       /*
-	    * '-' La operacion de resta.
-	    */
-       $('#op-minus').click(function(event){
-           $('#edit-display').val($('#edit-display').val() + '-') ;
-           nuevo = true;
-           event.preventDefault();
-       });
+        /*
+        * '-' La operacion de resta.
+        */
+        $('#op-minus').click(function(event){
+            $('#edit-display').val($('#edit-display').val() + '-') ;
+            nuevo = true;
+            event.preventDefault();
+        });
        
-       /*
-	    * '*' La operacion de multiplicacion.
-	    */
-       $('#op-mul').click(function(event){
-           $('#edit-display').val($('#edit-display').val() + '*') ;
-           nuevo = true;
-           event.preventDefault();
-       });
+        /*
+        * '*' La operacion de multiplicacion.
+        */
+        $('#op-mul').click(function(event){
+            $('#edit-display').val($('#edit-display').val() + '*') ;
+            nuevo = true;
+            event.preventDefault();
+        });
        
-       /*
-	    * '/' La operacion de division.
-	    */
-       $('#op-div').click(function(event){
-           $('#edit-display').val($('#edit-display').val() + '/') ;
-           nuevo = true;
-           event.preventDefault();
-       });
-		
-		/*
-		 * '=' La operacion de igual. Devuelve el resultado de una operacion
-	     */
-       $('#op-equ').click(function(event){
-		   var respuesta = eval($('#edit-display').val());
-           $('#edit-display').val(respuesta);
-           nuevo = false;
-           event.preventDefault();
-       });
-       
-		/*
-	     * 'CLR' Borra un numero a la vez, usualmente solo el ultimo.
-	     */
-       $('#op-Clr').click(function(event){
-           var num = $('#edit-display').val();
-           var newvalor='';
-           for(i=0; i<num.length - 1; i++)
-           {  newvalor += num[i]; }
-           $('#edit-display').val(newvalor);
-           event.preventDefault();
-       });
-      
+        /*
+        * '/' La operacion de division.
+        */
+        $('#op-div').click(function(event){
+            $('#edit-display').val($('#edit-display').val() + '/') ;
+            nuevo = true;
+            event.preventDefault();
+        });
+    
+        /*
+        * '=' La operacion de igual. Devuelve el resultado de una operacion
+        */
+        $('#op-equ').click(function(event){
+        var respuesta = eval($('#edit-display').val());
+            $('#edit-display').val(respuesta);
+            nuevo = false;
+            event.preventDefault();
+        });
+        
+        /*
+        * 'CLR' Borra un numero a la vez, usualmente solo el ultimo.
+        */
+        $('#op-Clr').click(function(event){
+            var num = $('#edit-display').val();
+            var newvalor='';
+            for(i=0; i<num.length - 1; i++)
+                newvalor += num[i];
+            $('#edit-display').val(newvalor);
+            event.preventDefault();
+        });
     });
-}) (jQuery);
+})(jQuery);
--- src/Form/CalculatorForm.php
+++ src/Form/CalculatorForm.php
@@ -7,10 +7,10 @@
 
 namespace Drupal\calculator\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormInterface;
 
-class CalculatorForm extends ConfigFormBase
+class CalculatorForm implements FormInterface
 {
 
   /**
@@ -25,232 +25,237 @@ class CalculatorForm extends ConfigFormBase
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-     $form['#attached']['js'] = array(
-         drupal_get_path('module', 'calculator').'/js/calculator.js',
-         drupal_get_path('module', 'calculator').'/js/styles.js' 
-     );
-     $form['#attached']['css'] = array(
-         drupal_get_path('module', 'calculator').'/css/dojo.css' 
-     );
+    $form['#attached']['js'] = array(
+      drupal_get_path('module', 'calculator').'/js/calculator.js',
+    );
 
-     $config = $this->config('calculator.calculator_form_config');
+    $form['#attached']['css'] = array(
+      drupal_get_path('module', 'calculator').'/css/calculator.css' 
+    );
+
     $form['display'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Display'),
-      '#description' => $this->t(''),
-      '#default_value' => $config->get('display'),
+      '#title' => '',
+      '#description' => '',
+      '#default_value' => "",
       '#disabled' =>  TRUE,
     ];
     
-
     $form['7'] = [
-        '#type' => 'button',
-        '#value' => '7',
-        '#title' => $this->t('7'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'7',
-        ),
-        '#default_value' => $config->get('7'),
-        '#prefix' => '<div class = "numeros"><div class = "789">',
+      '#type' => 'button',
+      '#value' => '7',
+      '#title' => t('7'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'7',
+      ),
+      '#default_value' => '7',
+      '#prefix' => '<div class = "numeros"><div class = "789">',
+    ];
+    
+    $form['8'] = [
+      '#type' => 'button',
+      '#value' => '8',
+      '#title' => t('8'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'8',
+      ),
+      '#default_value' => '8',
+    ];
+    
+    $form['9'] = [
+      '#type' => 'button',
+      '#value' => '9',
+      '#title' => t('9'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'9',
+      ),
+      '#default_value' => '9',
+      '#suffix' => '</div>',
+    ];
+    
+    $form['4'] = [
+      '#type' => 'button',
+      '#value' => '4',
+      '#title' => t('4'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'4',
+      ),
+      '#default_value' => '4',
+      '#prefix' => '<div class = "456">',
+    ];
+      
+    $form['5'] = [
+      '#type' => 'button',
+      '#value' => '5',
+      '#title' => t('5'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'5',
+      ),
+      '#default_value' => '5',
+    ];
+    
+    $form['6'] = [
+      '#type' => 'button',
+      '#value' => '6',
+      '#title' => t('6'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'6',
+      ),
+      '#default_value' => '6',
+      '#suffix' => '</div>',
       ];
-      $form['8'] = [
-        '#type' => 'button',
-        '#value' => '8',
-        '#title' => $this->t('8'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'8',
-        ),
-        '#default_value' => $config->get('8'),
-        
-      ];
-      $form['9'] = [
-        '#type' => 'button',
-        '#value' => '9',
-        '#title' => $this->t('9'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'9',
-        ),
-        '#default_value' => $config->get('9'),
-        '#suffix' => '</div>',
-      ];
-      $form['4'] = [
-        '#type' => 'button',
-        '#value' => '4',
-        '#title' => $this->t('4'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'4',
-        ),
-        '#default_value' => $config->get('4'),
-        '#prefix' => '<div class = "456">',
-      ];
-      $form['5'] = [
-        '#type' => 'button',
-        '#value' => '5',
-        '#title' => $this->t('5'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'5',
-        ),
-        '#default_value' => $config->get('5'),
-      ];
-      $form['6'] = [
-        '#type' => 'button',
-        '#value' => '6',
-        '#title' => $this->t('6'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'6',
-        ),
-        '#default_value' => $config->get('6'),
-        '#suffix' => '</div>',
-      ];
-      $form['1'] = [
-        '#type' => 'button',
-        '#value' => '1',
-        '#title' => $this->t('1'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'1',
-        ),
-        '#default_value' => $config->get('1'),
-        '#prefix' => '<div class = "123">',
-      ];
-      $form['2'] = [
-        '#type' => 'button',
-        '#value' => '2',
-        '#title' => $this->t('2'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'2',
-        ),
-        '#default_value' => $config->get('2'),
-      ];
-      $form['3'] = [
-        '#type' => 'button',
-        '#value' => '3',
-        '#title' => $this->t('3'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'3',
-        ),
-        '#default_value' => $config->get('3'),
-        '#suffix' => '</div>',
-      ];
+      
+    $form['1'] = [
+      '#type' => 'button',
+      '#value' => '1',
+      '#title' => t('1'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'1',
+      ),
+      '#default_value' => '1',
+      '#prefix' => '<div class = "123">',
+    ];
+    
+    $form['2'] = [
+      '#type' => 'button',
+      '#value' => '2',
+      '#title' => t('2'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'2',
+      ),
+      '#default_value' => '2',
+    ];
+    
+    $form['3'] = [
+      '#type' => 'button',
+      '#value' => '3',
+      '#title' => t('3'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'3',
+      ),
+      '#default_value' => '3',
+      '#suffix' => '</div>',
+    ];
 
     $form['0'] = [
-        '#type' => 'button',
-        '#value' => '0',
-        '#title' => $this->t('0'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'edit-'.'0',
-        ),
-        '#default_value' => $config->get('0'),
-        '#prefix' => '<div class = "cero-pto">',
-      ];
+      '#type' => 'button',
+      '#value' => '0',
+      '#title' => t('0'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'edit-'.'0',
+      ),
+      '#default_value' => '0',
+      '#prefix' => '<div class = "cero-pto">',
+    ];
 
-      $form['pto'] = [
-        '#type' => 'button',
-        '#value' => '.',
-        '#title' => $this->t('pto'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-'.'pto',
-        ),
-        '#default_value' => $config->get('pto'),
-        '#suffix' => '</div></div>',
-      ];
+    $form['pto'] = [
+      '#type' => 'button',
+      '#value' => '.',
+      '#title' => t('pto'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-'.'pto',
+      ),
+      '#default_value' => 'pto',
+      '#suffix' => '</div></div>',
+    ];
 
     $form['+'] = [
-        '#type' => 'button',
-        '#value' => '+',
-        '#title' => $this->t('+'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-plus',
-        ),
-        '#default_value' => $config->get('+'),
-        '#prefix' => '<div class = "oper"><div class = "+-">',
-      ];
+      '#type' => 'button',
+      '#value' => '+',
+      '#title' => t('+'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-plus',
+      ),
+      '#default_value' => '+',
+      '#prefix' => '<div class = "oper"><div class = "+-">',
+    ];
 
     $form['-'] = [
-        '#type' => 'button',
-        '#value' => '-',
-        '#title' => $this->t('-'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-minus',
-        ),
-        '#default_value' => $config->get('+'),
-        '#suffix' => '</div>',
-      ];
+      '#type' => 'button',
+      '#value' => '-',
+      '#title' => t('-'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-minus',
+      ),
+      '#default_value' => '+',
+      '#suffix' => '</div>',
+    ];
 
     $form['*'] = [
-        '#type' => 'button',
-        '#value' => '*',
-        '#title' => $this->t('*'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-mul',
-        ),
-        '#default_value' => $config->get('*'),
-        '#prefix' => '<div class = "*/">',
-      ];
+      '#type' => 'button',
+      '#value' => '*',
+      '#title' => t('*'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-mul',
+      ),
+      '#default_value' => '*',
+      '#prefix' => '<div class = "*/">',
+    ];
 
     $form['/'] = [
-        '#type' => 'button',
-        '#value' => '/',
-        '#title' => $this->t('/'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-div',
-        ),
-        '#default_value' => $config->get('/'),
-        '#suffix' => '</div>',
-      ];
+      '#type' => 'button',
+      '#value' => '/',
+      '#title' => t('/'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-div',
+      ),
+      '#default_value' => '/',
+      '#suffix' => '</div>',
+    ];
       
     $form['='] = [
-        '#type' => 'button',
-        '#value' => '=',
-        '#title' => $this->t('='),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-equ',
-        ),
-        '#default_value' => $config->get('='),
-        '#prefix' => '<div class = "=">',
-        '#suffix' => '</div>',
-      ];
+      '#type' => 'button',
+      '#value' => '=',
+      '#title' => t('='),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-equ',
+      ),
+      '#default_value' => '=',
+      '#prefix' => '<div class = "=">',
+      '#suffix' => '</div>',
+    ];
       
-      $form['AC'] = [
-        '#type' => 'button',
-        '#value' => 'AC',
-        '#title' => $this->t('AC'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-'.'AC',
-        ),
-        '#default_value' => $config->get('AC'),
-        '#prefix' => '<div class = "AC-Clr">',
-      ];
+    $form['AC'] = [
+      '#type' => 'button',
+      '#value' => 'AC',
+      '#title' => t('AC'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-'.'AC',
+      ),
+      '#default_value' => 'AC',
+      '#prefix' => '<div class = "AC-Clr">',
+    ];
 
     $form['Clr'] = [
-        '#type' => 'button',
-        '#value' => 'Clr',
-        '#title' => $this->t('Clr'),
-        '#description' => $this->t(''),
-        '#attributes' => array(
-            'id' => 'op-'.'Clr',
-        ),
-        '#default_value' => $config->get('Clr'),
+      '#type' => 'button',
+      '#value' => 'Clr',
+      '#title' => t('Clr'),
+      '#description' => '',
+      '#attributes' => array(
+        'id' => 'op-'.'Clr',
+      ),
+      '#default_value' => 'Clr',
         '#suffix' => '</div></div>',
-      ];        
+    ];        
     
-    return parent::buildForm($form, $form_state);
+    return $form;
   }
 
   /**
@@ -265,10 +270,5 @@ class CalculatorForm extends ConfigFormBase
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-
-    $this->config('calculator.calculator_form_config')
-          ->set('display', $form_state->getValue('display'))
-          ->set('1', $form_state->getValue('1'))
-        ->save();
   }
 }
